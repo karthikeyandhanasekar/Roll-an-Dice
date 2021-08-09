49e13317784beb4596439a8211e58eff72f689d5
Karthik <dkkarthik2000@gmail.com>
2021-08-09 20:01:26 +0530

    Roll Dice

--- js/index.js
+++ js/index.js
@@ -1,33 +1,47 @@
 
-var guess = Math.floor(Math.random()*100) +1 
 // multiply by limit that means 100 here
 //alert(guess)
-
+var count= parseInt(0)
+var target = 30
 function calc()
 {
-    if (document.getElementById("number").value === '')
-    {
-        document.getElementById("ans").innerHTML = "Given an number"
-        return false
-
-    }
-
-    var n= parseInt( document.getElementById("number").value)
     
-    if ( n > 100 && n <=0)
-    {
-    document.getElementById("ans").innerHTML = "Give an valid number bw 1 and 100"
-        return false
-    }
-    else if (n === guess)
-    document.getElementById("ans").innerHTML = "Awesome ! You got an lucky number  &#128526"
-    else if (n > guess)
-    document.getElementById("ans").innerHTML = "So close ! Try giving lesser number &#128517"
-    else if (n < guess)
-    document.getElementById("ans").innerHTML = "So close ! Try giving greater number &#128517"
+    count+=1
 
-    else
-    document.getElementById("ans").innerHTML = "It ok , Don't Give Up."
+    player1 = document.getElementById("player1")
+    player2 = document.getElementById("player2")
+    //player2
+    if (count%2 == 0)
+    {
+        var dice2 = Math.floor(Math.random()*6) +1 
+
+        //alert("even player2")
+        player2.value =  parseInt(player2.value)+  dice2
+        if (player2.value  >= target)
+        {
+            document.getElementById("ans").innerHTML = "Player 2 Wins"
+            docu
+        }
+    }
+    if (count%2 != 0)
+    {
+        //alert("odd player1")
+        var dice1 = Math.floor(Math.random()*6) +1 
+
+        player1.value =  parseInt(player1.value)+  dice1
+        if (player2.value  >= target)
+        {
+            document.getElementById("ans").innerHTML = "Player 1 Wins"
+        }
+
+
+    }
+
+
+    
+
+
+
 
 
 
